0b33f7a8f4c1ddc5007e71485971213e72b0fa0b
Raman <ramanpreet.h64@gmail.com>
2020-09-10 16:35:09 +0530

    main method read kar pinto

---
+++ TestCommonGit/src/com/lti/Test2.java
@@ -0,0 +1,10 @@
+package com.lti;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("naya project error k saath create ho ra hai, jdk build path mein add krne pe project sahi hota hai");
+	}
+
+}
